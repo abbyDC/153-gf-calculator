2a8e73e6ef5931173e512a1da1b1f4a256cfa560
AbbyDC <chub_009@yahoo.com>
2017-05-25 13:43:40 +0800

    cleanup files

--- delcastillo_gf_calcu.py
+++ delcastillo_gf_calcu.py
@@ -8,26 +8,50 @@ def pad_poly(poly,len_a,len_b):
 		poly.insert(0,0)
 	return poly
 
+def del_zeroes(a):
+	new_a=a
+	if (len(a)>0):
+		while(new_a[0]==0):
+			new_a.pop(0)
+	return new_a
 
+def print_output(poly,a):
+	ctr = len(a)-1
+	print poly,
+	for x in range(0,len(a)):
+		if (x == len(a)-1):
+			print str(a[x])
+		else:
+			print str(a[x]) + "x^" + str(ctr) + " +",
+		ctr-=1
+
+def input_check(a):
+	b = a.split()
+	for x in b:
+		if (x.isdigit()==False):
+			return 0
+	return 1
+
+#Polynomial Operations
 def poly_add(a,b):
 	result = []
+	# print "\nDetailed Solution:"
+	# print "Perform bitwise xor operation for every term in A(x) and B(x)."
 	for x in range(len(a)):
 		xor_value = a[x] ^ b[x]
+		# print "if we perform",str(a[x]),"xor",str(b[x]),", we get",str(xor_value)
 		result.append(xor_value)
 	return result
 
 def poly_subtract(a,b):
 	result = []
+	print "\nDetailed Solution:"
+	print "Perform bitwise xor operation for every term in A(x) and B(x)."
 	for x in range(len(a)):
 		xor_value = a[x] ^ b[x]
+		print "if we perform",str(a[x]),"xor",str(b[x]),", we get",str(xor_value)
 		result.append(xor_value)
 	return result
-
-def del_zeroes(a):
-	new_a=a
-	while(new_a[0]==0):
-		new_a.pop(0)
-	return new_a
 
 def s_mult(a,b,c):
 	a=list(bin(a)[2:])
@@ -51,10 +75,10 @@ def s_mult(a,b,c):
 
 	#mod P(x) as long as the resulting polynomial is longer than P(x)
 	#delete leading zeroes
-		#pops the zero in front of binary
 	temp = int(''.join(map(str,temp)),2)
 	temp= list(bin(temp)[2:])
 	temp=[int(n) for n in temp]
+
 	temp_c=[int(n) for n in c]
 	len_c = len(c)
 	while(len(temp)>=len_c):
@@ -62,7 +86,7 @@ def s_mult(a,b,c):
 		temp_c.extend("0"*(len(temp)-len(temp_c)))
 		temp_c=[int(n) for n in temp_c]
 		temp=poly_add(temp,temp_c)
-		#pops the zero in front of binary
+		#delete leading zeroes
 		temp = int(''.join(map(str,temp)),2)
 		temp= list(bin(temp)[2:])
 		temp=[int(n) for n in temp]
@@ -72,9 +96,9 @@ def s_mult(a,b,c):
 def poly_multiply(a,b,c):
 	temp=[]
 	#multiply each element of b to a
-	temp_c=c
+	temp_c=[int(n) for n in c]
 	for x in range(len(b)-1,-1,-1):
-		temp_c=c
+		temp_c=[int(n) for n in c]
 		prod=[str(s_mult(a[y],b[x],temp_c)) for y in range(len(a))] #temporary storage for current product
 		prod.extend("0"*((len(b)-1)-x)) #pad-right depending on which place we are multiplying
 		prod=[int(n) for n in prod]
@@ -99,40 +123,48 @@ def poly_multiply(a,b,c):
 
 	return temp
 
-def poly_divide(a,b):
-	result = 0
-	'''insert operation here'''
+def div_mult(ctr,a,c):
+		prod=[str(s_mult(a[y],ctr,c)) for y in range(len(a))] #temporary storage for current product
+		prod=[int(n) for n in prod]
+		return prod
+
+def poly_divide(a,b,c):
+	result=[]
+	ctr = 1
+	x = div_mult(ctr,b,c)
+	len_b = len(b)
+	while (len(a)>=len_b):
+		while(a[0]!=x[0]):
+			ctr+=1
+			x = div_mult(ctr,b,c)
+
+		result.append(ctr)
+		if (len(a) < len(b)):
+			return result
+		else: 
+			len_x = len(x)
+			x.extend("0"*((len(a))-len_x))
+			x=[int(n) for n in x]
+			a = poly_add(a,x)
+			a = del_zeroes(a)
+		ctr=1
 	return result
 
-def print_output(poly,a):
-	ctr = len(a)-1
-	print poly,
-	for x in range(0,len(a)):
-		if (x == len(a)-1):
-			print str(a[x])
-		else:
-			print str(a[x]) + "x^" + str(ctr) + " +",
-		ctr-=1
-
-def input_check(a):
-	b = a.split()
-	for x in b:
-		if (x.isdigit()==False):
-			return 0
-	return 1
-
+'''Main Program'''
 #User Input
 a_input = raw_input("Enter A(x) separated by spaces: ")
 while (input_check(a_input)!=1):
 	print "You have entered a non-digit in the polynomial."
 	a_input = raw_input("Enter A(x) separated by spaces: ")
 a_input = [int(n) for n in a_input.split()]
+a_div = [int(n) for n in a_input]
 
 b_input = raw_input("Enter B(x) separated by spaces: ")
 while (input_check(b_input)!=1):
 	print "You have entered a non-digit in the polynomial."
 	b_input = raw_input("Enter B(x) separated by spaces: ")
 b_input = [int(n) for n in b_input.split()]
+b_div = [int(n) for n in b_input]
 
 c_input = raw_input("Enter P(x) separated by spaces: ")
 while (input_check(c_input)!=1):
@@ -140,7 +172,6 @@ while (input_check(c_input)!=1):
 	c_input = raw_input("Enter P(x) separated by spaces: ")
 c_input = [int(n) for n in c_input.split()]
 
-#Main Program
 #preprocess input
 if len(a_input)>len(b_input):
 	b_input = pad_poly(b_input,len(a_input),len(b_input))
@@ -160,24 +191,28 @@ while(operation<1 or operation>4):
 result = 0
 if operation == 1:
 	result = poly_add(a_input,b_input)
+	print "\nOutput:"
 	print_output("A(x) =",a_input)
 	print_output("B(x) =",b_input)
 	print_output("A(x) + B(x) =",result)
 
 elif operation == 2:
 	result = poly_subtract(a_input,b_input)
+	print "\nOutput:"
 	print_output("A(x) =",a_input)
 	print_output("B(x) =",b_input)
 	print_output("A(x) - B(x) =",result)
 
 elif operation == 3:
 	result = poly_multiply(a_input,b_input,c_input)
+	print "\nOutput:"
 	print_output("A(x) =",a_input)
 	print_output("B(x) =",b_input)
 	print_output("A(x) x B(x) =",result)
 
 elif operation == 4:
-	result = poly_divide(a_input,b_input)
+	result = poly_divide(a_div,b_div,c_input)
+	print "\nOutput:"
 	print_output("A(x) =",a_input)
 	print_output("B(x) =",b_input)
 	print_output("A(x) / B(x) =",result)
---
+++ delcastillo_gf_calcu.spec
@@ -0,0 +1,33 @@
+# -*- mode: python -*-
+
+block_cipher = None
+
+
+a = Analysis(['delcastillo_gf_calcu.py'],
+             pathex=['/home/abbydc/Desktop/git_files/acads/153-gf-calculator'],
+             binaries=[],
+             datas=[],
+             hiddenimports=[],
+             hookspath=[],
+             runtime_hooks=[],
+             excludes=[],
+             win_no_prefer_redirects=False,
+             win_private_assemblies=False,
+             cipher=block_cipher)
+pyz = PYZ(a.pure, a.zipped_data,
+             cipher=block_cipher)
+exe = EXE(pyz,
+          a.scripts,
+          exclude_binaries=True,
+          name='delcastillo_gf_calcu',
+          debug=False,
+          strip=False,
+          upx=True,
+          console=True )
+coll = COLLECT(exe,
+               a.binaries,
+               a.zipfiles,
+               a.datas,
+               strip=False,
+               upx=True,
+               name='delcastillo_gf_calcu')
